82503a187de08a71d164c8b1adf6c64915570381
alec178 <32443068+alec178@users.noreply.github.com>
2017-10-02 08:55:01 +0800

    Create t01.py

---
+++ t01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+cash_div_history = pd.DataFrame()
+
+url='http://www.wantgoo.com/stock/report/basic_dp?StockNo=2317'
+url1='http://www.wantgoo.com/stock/twstock/threebuysellrank?type=%E5%A4%96%E8%B3%87' #外資買賣超排行榜
+
+cash_div_history = pd.read_html(url)
+         
+cash_db = cash_div_history[0]
+print cash_db
+
+print cash_db.iloc[0:5, 0]
+
+# for i in range(5):
+# 	print cash_db.loc[i][u'股票股利'].encode('utf-8'):
+
